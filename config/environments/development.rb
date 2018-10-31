a130942ba868a38bfd01f7cc9cb3ba41c3409587
Mildred Silva <mildred_natali@hotmail.com>
2018-10-31 17:01:38 -0600

    Comment removed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,4 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
-  #Image active_storage
-  
 end
