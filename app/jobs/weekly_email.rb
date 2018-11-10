58575e4a677a5dd5ee7a937aeef8a74fa6a1d59d
Mildred Silva <mildred_natali@hotmail.com>
2018-11-09 20:28:54 -0600

    fixing class weeklyemail

--- app/jobs/weekly_email.rb
+++ app/jobs/weekly_email.rb
@@ -2,12 +2,12 @@ class WeeklyEmail < ApplicationJob
   #sidekiq_options retry: false
    queue_as :default
    def perform(*)
-    #@users = User.all
-    #@users.each do |user|
-     # mail = ApplicationMailer.sample_email(id: user.id, email: user.email)
-      #mail.deliver_now
+    @users = User.all
+    users.each do |user|
+      mail = ApplicationMailer.sample_email(id: user.id, email: user.email)
+      mail.deliver_now
 
-    #end
-    puts "Hola desde weeklymailer"
+    end
+    
   end
 end
