b618d0072b53d4c48e7ea7331f1ac79bcc221fae
Mildred Silva <mildred_natali@hotmail.com>
2018-11-13 13:52:21 -0600

    Adding second parameter on sample_email

--- app/jobs/weekly_email.rb
+++ app/jobs/weekly_email.rb
@@ -4,7 +4,7 @@ class WeeklyEmail < ApplicationJob
    def perform(*)
     @users = User.all
     users.each do |user|
-      mail = ApplicationMailer.sample_email(id: user.id, email: user.email)
+      mail = ApplicationMailer.sample_email(email: user.id, email: user.email)
       mail.deliver_now
     end
   end
