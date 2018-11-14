f269045bbefaa176205b4b669878af73bd1e58e3
Mildred Silva <mildred_natali@hotmail.com>
2018-11-14 11:33:36 -0600

    fixing an error of a parameter on weekly_email

--- app/jobs/weekly_email.rb
+++ app/jobs/weekly_email.rb
@@ -4,7 +4,7 @@ class WeeklyEmail < ApplicationJob
    def perform(*)
     @users = User.all
     users.each do |user|
-      mail = ApplicationMailer.sample_email(id: user.id, email: user.email)
+      mail = ApplicationMailer.sample_email(user)
       mail.deliver_now
     end
   end
